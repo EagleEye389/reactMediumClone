8420daa8b9eeb98a47b2d3e3bd84f5e418ba0d0a
Deepak Gupta <deepakgupta@deepak3151918.local>
2020-07-10 21:07:07 +0530

    updating server js

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const path = require('path')
 const port = process.env.PORT || 3001
 
 app.use(express.static(path.join(__dirname, 'dist')));
+app.use(require('connect-history-api-fallback')()) 
+app.use(express.static(path.join(__dirname, 'dist')));
+
 app.get('*.js', function(req, res, next) {
   req.url = req.url + '.gz';
   res.set('Content-Encoding', 'gzip');
