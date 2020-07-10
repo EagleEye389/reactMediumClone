39aed436b6d8cf76eabd9a83769215cce52037da
Deepak Gupta <deepakgupta@deepak3151918.local>
2020-07-10 21:14:31 +0530

    updating server js

--- server.js
+++ server.js
@@ -2,13 +2,8 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const port = process.env.PORT || 3001
-const history =  require('connect-history-api-fallback')
 
 app.use(express.static(path.join(__dirname, 'dist')));
-app.use(history({
-  disableDotRule: true,
-  verbose: true
-})) 
 
 app.get('*.js', function(req, res, next) {
   req.url = req.url + '.gz';
